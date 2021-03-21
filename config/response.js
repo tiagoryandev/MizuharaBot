83a7b43c208f59b8a182d322e4061f1ddb9a85ef
TiaGoiNsaNy <62999761+TiaGoiNsaNy@users.noreply.github.com>
2021-03-21 11:42:04 -0300

    :eyes: Arquivo para armazenamento de respostas.

---
+++ config/response.js
@@ -0,0 +1,20 @@
+let response = ["Sim.",
+    "Não.",
+    "Talvez.",
+    "Não posso opinar sobre isso...",
+    "Sei de nada...",
+    "Provavelmente sim.",
+    "Provavelmente não.",
+    "Não sei...",
+    "Eu diria que sim.",
+    "Eu diria que não.",
+    "Hmm acho que não.",
+    "Ah sei de nada não...fui!",
+    "Isso é meio obvio.",
+    "Concerteza!",
+    "Pergunta para a Sumi.",
+    "Pergunta para a Ruka.",
+    "Pergunta para a Mizuhara"
+]; // Define um array com varias respostas
+
+module.exports = response; // Exporta o array
