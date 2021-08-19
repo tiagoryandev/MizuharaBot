a319ecc1d2e68e05e7feb1fb81e75d5588e7d2c2
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-19 12:27:12 +0800

    Delete config directory

--- config/blacklist.js
+++
@@ -1,3 +0,0 @@
-let blacklist = []; // Lista dos IDs de servidores banidos dos comandos em array
-
-module.exports = blacklist; // Exporta essa lista
--- config/developers.js
+++
@@ -1,3 +0,0 @@
-let devlist = ["568182075929395210"] // Lista de desenvolvedores em array
-
-module.exports = devlist; // Exporta a lista
